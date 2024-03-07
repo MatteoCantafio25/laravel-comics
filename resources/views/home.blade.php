454b02e103c665659510f3966d9587ea42e1fd70
MatteoCantafio25 <mattyteo004@gmail.com>
2024-03-07 17:00:13 +0100

    first structure

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Matteo Cantafio">
-    <meta name="description" content="Laravel Primi Passi">
-    <title>Laravel Primi Passi</title>
+    <meta name="description" content="Laravel Comics Layout">
+    <title>Laravel Comics Layout</title>
+    @vite('resources/js/app.js')
 </head>
-    <body>
-    </body>
+
+<body>
+</body>
 </html>
