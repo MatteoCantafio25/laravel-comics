8b9eeb69de263bb0105ca69ab81e296a738e06d7
MatteoCantafio25 <mattyteo004@gmail.com>
2024-03-07 15:57:59 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Matteo Cantafio">
+    <meta name="description" content="Laravel Primi Passi">
+    <title>Laravel Primi Passi</title>
+</head>
+    <body>
+    </body>
+</html>
